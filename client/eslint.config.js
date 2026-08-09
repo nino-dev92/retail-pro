a87fbc0092e49bb7b5eab226d83cdf207b2a0754
ninodev92 <nnejirichard@yahoo.com>
2026-08-09 10:28:43 +0100

    eslint

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,22 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+
+    rules: {
+      // We'll enable these later
+      "@typescript-eslint/no-explicit-any": "off",
+
+      // Warn instead of failing CI
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+
+      // Required for Express Request augmentation
+      "@typescript-eslint/no-namespace": "off",
+
+      // Allow String, Number, Boolean for now
+      "@typescript-eslint/no-wrapper-object-types": "off",
+
+      // Don't complain about CommonJS files like jest.config.cjs
+      "no-undef": "off",
     },
   },
-])
+]);
