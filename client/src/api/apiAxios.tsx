32a0319fe385aa18cc932cc1fd4f133a366909fa
ninodev92 <nnejirichard@yahoo.com>
2026-08-19 07:27:53 +0100

    added backend api

--- client/src/api/apiAxios.tsx
+++ client/src/api/apiAxios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiAxios = axios.create({
-  baseURL: "https://retail-pro-server.onrender.com",
+  baseURL: "https://retail-pro-server.onrender.com/api",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
